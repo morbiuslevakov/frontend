a8bb23382d9a954f525b82d4bba3d0d6a67b1b6b
morbiuslevakov <143098039+morbiuslevakov@users.noreply.github.com>
2024-03-05 23:14:21 +0300

    Update SharedLayout.jsx

--- src/pages/shared-layout/SharedLayout.jsx
+++ src/pages/shared-layout/SharedLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Box } from '@mui/material'
-import Header from '../../components/header/Header'
+import Header from '../../components/Header/Header'
 
 export const SharedLayout = () => {
   return (
