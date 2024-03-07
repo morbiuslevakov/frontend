6078f6087e8499bf906ec1ef2829746af022afda
NikitaShevchik <n.shevchik@softteco.com>
2024-03-07 22:28:47 +0300

    fix(type): change type handling in orders on p2p buy and sell pages

--- src/components/p2p-page/p2p-trade-item/TradeItemHeader.jsx
+++ src/components/p2p-page/p2p-trade-item/TradeItemHeader.jsx
@@ -3,8 +3,10 @@ import { Stack, Typography } from '@mui/material'
 import { OrderActionWrapper } from '../../orderCreate-page/Styled'
 import { actionType } from '../../../utils/constants/order-create'
 import { FormStackSection } from '../../form/FormStackSection'
+import { switchType } from '../../../utils/p2p-utils'
 
 export const TradeItemHeader = ({ states, oneTokenPrice }) => {
+  const stringType = switchType(states.type)
   return (
     <FormStackSection >
       <Stack>
@@ -12,7 +14,7 @@ export const TradeItemHeader = ({ states, oneTokenPrice }) => {
         <Typography variant='gray'>Цена за 1 {states.crypto}</Typography>
       </Stack>
       <OrderActionWrapper>
-        <Typography fontWeight={600} variant='blue'>{actionType[states.type]}</Typography>
+        <Typography fontWeight={600} variant='blue'>{actionType[stringType]}</Typography>
       </OrderActionWrapper>
     </FormStackSection>
   )
