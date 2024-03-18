4d3a57961186f4777a1eb7744ea3c72f8ef1ebef
NikitaShevchik <n.shevchik@softteco.com>
2024-03-18 17:09:09 +0300

    feat(new change password form): change for for edit password with validation and cancel button

--- src/components/profile-page/Security.jsx
+++ src/components/profile-page/Security.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Button, Stack, Typography } from '@mui/material'
 import { ProfileCard } from './Styled'
 import { ChangePassword } from './ChangePassword'
+import { useMediaQueryHook } from "../../hooks/use-media-query.hook";
 
 export const Security = () => {
+  const isMobile = useMediaQueryHook('sm')
+  const width = isMobile ? '100%' : "300px"
+
   return (
     <ProfileCard>
       <Stack gap={2}>
         <Typography fontSize={26} lineHeight={1}>Безопасность</Typography>
-        <Stack gap={1} width={'200px'}>
+        <Stack gap={1} width={width}>
           <ChangePassword />
           <Button variant="outlined" disabled>Включить F2A</Button>
         </Stack>
