bf6d1ffb6db46c911f501cf8ac50ab3a41e6975e
NikitaShevchik <n.shevchik@softteco.com>
2024-03-18 16:45:31 +0300

    feat(p2p empty): add empty text for p2p

--- src/components/p2p-page/OptionsAndList.jsx
+++ src/components/p2p-page/OptionsAndList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { P2PFormOptions } from './P2PFormOptions'
-import { CircularProgress, Stack } from '@mui/material'
+import { CircularProgress, Stack, Typography } from '@mui/material'
 import { P2PTradeItem } from './p2p-trade-item/P2PTradeItem'
+import { FormContentWrapper } from '../orderCreate-page/Styled'
 
 export const OptionsAndList = ({ states, setState }) => {
+  const isEmpty = states.orders.length;
+
   return (
     <>
       <P2PFormOptions states={states} setState={setState} />
@@ -11,6 +14,7 @@ export const OptionsAndList = ({ states, setState }) => {
         {states.orders.map(order => {
           return <P2PTradeItem key={order.id} states={states} order={order} setOrder={setState.order} setStep={setState.step} />
         })}
+        {!!!isEmpty && <FormContentWrapper mb={2}><Typography textAlign={'center'}>Пока нет открытых объявлений</Typography></FormContentWrapper>}
       </Stack>}
     </>
   )
